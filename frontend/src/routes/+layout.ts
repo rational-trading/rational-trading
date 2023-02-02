916014489f809e3db5b6e64d8a12999a043ea8d3
Miles Watson <contact@mileswatson.net>
2023-02-02 21:00:46 +0000

    Build directly to backend static folder.

--- frontend/src/routes/+layout.ts
+++ frontend/src/routes/+layout.ts
@@ -1,1 +1,4 @@
+// required for static export
 export const prerender = true;
+// allows us to omit '.html' extensions in URL paths when serving with whitenoise
+export const trailingSlash = "always";
