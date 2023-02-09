28b37690bd0eef8e24a0e9fd631822c0d21b4e71
katelkq <102189165+katelkq@users.noreply.github.com>
2023-02-09 11:42:27 +0000

    Navbar and site layout (#36)
    * Test
    
    * Added navbar
    
    * Removed irrelevant code; added some color schemes
    
    * Working on site layout; added logo dependencies
    
    * Updated color scheme
    
    * Try to fix build failure on Github
    
    * Try again
    
    * And again...
    
    * More work on page layout
    
    * Watchlist & news layout (?)
    
    * Scrollability within sections
    
    * Minor Improvements (#40)
    
    * Improvements.
    
    * Added "clickable" table rows.
    
    * Made dot bigger.
    
    * General layout completed
    
    * Linting issues...
    
    * const sentiment
    
    * package-lock.json gone
    
    ---------
    
    Co-authored-by: Miles Watson <contact@mileswatson.net>

---
+++ frontend/src/routes/portfolio/+page.svelte
@@ -0,0 +1,210 @@
+<div class="block mt-5 ml-5">
+    <h1 class="title is-2">Portfolio</h1>
+</div>
+
+<div class="columns mt-5" style="width: calc(100vw + 12px);">
+    <div class="column is-two-fifths">
+        <h2 class="title is-5 ml-5">Summary</h2>
+        <div class="box mx-5 has-background-grey-darker" style="height: 25vh;">
+            <div>
+                <p class="heading">Current value</p>
+                <p class="title">£13,267.39</p>
+            </div>
+            <div class="columns mt-5">
+                <div class="column">
+                    <div>
+                        <p class="heading">Today's gain/loss</p>
+                        <p class="subtitle has-text-success">£28.27 (1.50%)</p>
+                    </div>
+                </div>
+                <div class="column">
+                    <div>
+                        <p class="heading">Overall gain/loss</p>
+                        <p class="subtitle has-text-success">
+                            £1,937.25 (14.60%)
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="column">
+        <h2 class="title is-5 ml-5">Recent Activities</h2>
+        <div
+            class="box mx-5 py-2 has-background-grey-darker"
+            style="height: 25vh;"
+        >
+            <div
+                class="table-container"
+                style="height: 100%; overflow-y: scroll;"
+            >
+                <table class="table is-fullwidth is-dark">
+                    <thead>
+                        <tr>
+                            <th class="has-text-left">Time</th>
+                            <th class="has-text-left">Symbol</th>
+                            <th class="has-text-left">Side</th>
+                            <th class="has-text-left"
+                                ><abbr title="Quantity">Qty</abbr></th
+                            >
+                            <th>Price</th>
+                            <th>Total value</th>
+                            <th class="has-text-left">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                        <tr>
+                            <td class="has-text-left">2023-02-08 21:09:19</td>
+                            <th class="has-text-left">AAPL</th>
+                            <td class="has-text-left">Buy</td>
+                            <td class="has-text-left">10</td>
+                            <td class="has-text-right">151.66</td>
+                            <td class="has-text-right">1516.63</td>
+                            <td class="has-text-left has-text-danger"
+                                >Rejected</td
+                            >
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="block my-5">
+    <h2 class="title is-5 ml-5">Details</h2>
+    <div
+        class="box mx-5 has-background-grey-darker"
+        style="height: 38vh; overflow-y: auto;"
+    >
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut
+            dignissim libero. Maecenas elementum ipsum lectus, vel ornare libero
+            placerat id. Mauris ultricies, massa sed tempus tristique, quam
+            turpis aliquam tortor, in convallis risus nisl vel ipsum. Duis ante
+            arcu, vulputate et neque non, volutpat aliquet odio. Quisque eu erat
+            ornare nisl sodales venenatis. Vivamus id elementum massa. Etiam
+            posuere tortor vel sem volutpat, non dignissim leo ullamcorper.
+            Curabitur pulvinar magna et feugiat facilisis. Sed maximus leo mi,
+            id ultricies tortor rhoncus ut. Morbi sollicitudin arcu et congue
+            viverra. Proin eget iaculis enim. Vestibulum ante ipsum primis in
+            faucibus orci luctus et ultrices posuere cubilia curae; Proin nec
+            rutrum lacus, ac porta quam. Nullam sed ligula hendrerit, hendrerit
+            massa et, facilisis sapien.
+        </p>
+
+        <p>
+            Praesent vel libero nec ante faucibus convallis in at mi. Phasellus
+            et purus nisl. Vestibulum tortor nunc, vestibulum mollis finibus
+            eget, posuere in massa. Praesent consequat, leo consectetur faucibus
+            bibendum, justo eros porta odio, vitae ullamcorper dui leo ac erat.
+            Nunc maximus malesuada justo nec auctor. Cras vel sagittis lorem,
+            quis blandit tortor. Nunc odio nunc, posuere ut justo vitae,
+            consequat vestibulum est.
+        </p>
+        <p>
+            Integer ut egestas mauris. Donec scelerisque dui sit amet arcu
+            condimentum posuere. Nullam in facilisis lectus. Aenean pellentesque
+            vel sapien eget volutpat. Nullam pellentesque leo vel odio finibus
+            commodo. Proin id dolor eu metus finibus dapibus maximus id purus.
+            Nam volutpat lacus aliquam sem luctus iaculis. Sed convallis quam
+            quis aliquam facilisis. Suspendisse quis volutpat mauris.
+        </p>
+        <p>
+            Orci varius natoque penatibus et magnis dis parturient montes,
+            nascetur ridiculus mus. Nunc sit amet congue dui. Maecenas
+            pellentesque efficitur ullamcorper. Sed ornare nulla at dolor mattis
+            egestas. Proin posuere leo dolor, non sodales ante porta vitae.
+            Nulla turpis turpis, lacinia nec est non, laoreet placerat nunc.
+            Aliquam accumsan magna ac felis ornare, accumsan suscipit tellus
+            pellentesque. Pellentesque imperdiet consequat tellus nec bibendum.
+            Mauris ut varius lorem. Morbi pellentesque lorem sit amet turpis
+            fringilla commodo. Quisque consectetur placerat tortor non bibendum.
+            Curabitur at tempus risus. Sed maximus justo in consectetur aliquet.
+            In ante purus, dictum non turpis a, cursus dapibus ipsum. Praesent
+            rhoncus aliquet nunc, ac elementum est fermentum condimentum.
+        </p>
+
+        <p>
+            Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia curae; Mauris consectetur lacinia dui, vitae dictum
+            tortor elementum eu. Duis maximus vitae nisl egestas gravida. Nunc
+            hendrerit lorem ligula, vel volutpat odio posuere ac. Integer sapien
+            mauris, maximus ut risus eleifend, commodo aliquam metus. Curabitur
+            laoreet eleifend lacus. Proin sed augue turpis. Integer a velit
+            egestas quam venenatis rhoncus vel nec dolor. Vestibulum velit
+            lacus, sodales euismod eros eget, feugiat accumsan nisl. Nam arcu
+            lorem, maximus ut massa ut, hendrerit vehicula leo. Nullam finibus
+            tincidunt malesuada. Fusce blandit elementum urna, et pellentesque
+            sem sodales vitae. Duis dui est, ornare ut mi at, lacinia facilisis
+            mi. In hac habitasse platea dictumst.
+        </p>
+    </div>
+</div>
--- frontend/src/routes/shopping/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import Counter from "$components/Counter.svelte";
-
-    const items = ["Apples", "Bananas", "Oranges"];
-</script>
-
-{#each items as label}
-    <Counter {label} />
-{/each}
