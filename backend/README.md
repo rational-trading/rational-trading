d70dbb96736d65856204e2d84ab376bb818e4d7a
Miles Watson <contact@mileswatson.net>
2023-02-12 13:59:21 +0000

    Fixed typing.

--- backend/README.md
+++ backend/README.md
@@ -37,7 +37,7 @@ Note: Commands should be ran from this current folder.
 
 5) View the Swagger API docs by going to http://127.0.0.1:8000/api/docs#/. This page will allow you to view the available endpoints and make test requests in browser.
 
-6) Typecheck your Python code with `mypy .`
+6) Typecheck your Python code with `python check.py`
 
 7) Deactivate venv when finished working.
 
--- backend/polygon/polygon_api.py
+++ backend/polygon/polygon_api.py
@@ -3,17 +3,23 @@ Polygon API class to get stock financials and news articles
 https://polygon.io/docs/stocks/
 https://polygon-api-client.readthedocs.io/en/latest/index.html
 """
-from polygon import RESTClient # type: ignore
-import config #contains API_KEY
+from http.client import HTTPResponse
+from typing import Iterator
+from polygon import RESTClient
+from polygon.rest.models import StockFinancial
+from polygon.rest.models import TickerNews
+import config
+
 
 class PolygonAPI():
     def __init__(self) -> None:
-        self.client = RESTClient(api_key=config.API_KEY) # type: ignore
+        self.client = RESTClient(api_key=config.API_KEY)  # type: ignore
 
-    def get_financials(self, ticker: str) -> object:
-        financials = self.client.vx.list_stock_financials(ticker=ticker, limit=1, timeframe="annual", include_sources=True)
+    def get_financials(self, ticker: str) -> Iterator[StockFinancial] | HTTPResponse:
+        financials = self.client.vx.list_stock_financials(
+            ticker=ticker, limit=1, timeframe="annual", include_sources=True)
         return financials
-    
-    def get_news(self, ticker: str) -> object:
+
+    def get_news(self, ticker: str) -> Iterator[TickerNews] | HTTPResponse:
         news = self.client.list_ticker_news(ticker=ticker, limit=1)
         return news  # use next(news) to iterate over
