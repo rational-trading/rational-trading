9c9821784c0d9c4baeba9629d4f5e23ca9b13b9e
Simon <luo.simon@outlook.com>
2023-02-12 12:34:31 +0000

    Make mypy check happy

--- backend/polygon/polygon_api.py
+++ backend/polygon/polygon_api.py
@@ -10,10 +10,10 @@ class PolygonAPI():
     def __init__(self) -> None:
         self.client = RESTClient(api_key=config.API_KEY)
 
-    def get_financials(self, ticker: str):
+    def get_financials(self, ticker: str) -> object:
         financials = self.client.vx.list_stock_financials(ticker=ticker, limit=1, timeframe="annual", include_sources=True)
         return financials
     
-    def get_news(self, ticker: str):
+    def get_news(self, ticker: str) -> object:
         news = self.client.list_ticker_news(ticker=ticker, limit=1)
         return news  # use next(news) to iterate over
