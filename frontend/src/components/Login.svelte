256b56013704ed1cf6bf6aeb568ed041131b4b5b
Stefan Pitur <stefan.pitur24@gmail.com>
2023-02-13 01:27:15 +0000

    improvements on Initial Login

--- frontend/src/components/Login.svelte
+++ frontend/src/components/Login.svelte
@@ -24,12 +24,12 @@
     }
 </style>
 
-<button class="button is-light" on:click={() => { active = true; }}>
+<button class="button is-primary" on:click={() => { active = true; }}>
     <strong>Log in</strong>
 </button>
 
 {#if active}
-    <div class="modal {isActive()}">
+    <div class="modal is-active">
     <div class="modal-background" color=""/>
     <div class="modal-content">
         <div class="is-flex is-flex-direction-column">
@@ -37,16 +37,16 @@
                 <button class="delete is-large" style="background: #D9D9D9" aria-label="close" on:click={() => { active = false; }}></button>
             </div>
             <div class="is-flex is-flex-direction-column pt-4" id="login-contents">
-                <p class="title is-1 has-text-black">Log in</p>
+                <p class="title is-1 has-text-dark">Log in</p>
 
                 <div class="field mt-5">
                     <div class="control">
-                        <input class="input" type="email" placeholder="    Email" />
+                        <input class="input" type="email" placeholder="Email" />
                     </div>
                 </div>
                 <div class="field mt-3">
                     <div class="control">
-                        <input class="input" type="password" placeholder="    Password" />
+                        <input class="input" type="password" placeholder="Password" />
                     </div>
                 </div>
 
@@ -57,14 +57,14 @@
                 <div class="is-align-self-flex-end pt-5">
                     <div class="field">
                         <div class="control">
-                            <button class="button is-link is-medium"><p class="px-3">Log in</p></button> 
+                            <button class="button is-link is-medium"><p class="px-3"><strong>Log in</strong></p></button> 
                         </div>
                     </div>
                 </div>
 
 
                 <div class="is-align-self-center pt-6">
-                    <p class="has-text-black">Don't have an account? Sign up <a class="link" href="/">here</a>.</p>
+                    <p class="has-text-dark">Don't have an account? Sign up <a class="link" href="/">here</a>.</p>
                 </div>
             </div>
         </div>
