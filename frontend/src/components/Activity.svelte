e7483a445807bbb779f79e40b49574a1e8e76b14
Miles Watson <contact@mileswatson.net>
2023-02-13 17:57:58 +0000

    Reduced redundancy, simplified typing.

--- frontend/src/components/Activity.svelte
+++ frontend/src/components/Activity.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-    interface Activity {
-        time: string;
-        symbol: string;
-        side: string;
-        qty: number;
-        price: number;
-        totalVal: number;
-        status: "Filled" | "Pending" | "Rejected";
-    }
+    import type { Activity } from "$lib/types";
 
     export let data: Activity;
 
@@ -23,9 +15,10 @@
 <tr>
     <td class="has-text-left">{data.time}</td>
     <th class="has-text-left">{data.symbol}</th>
-    <td class="has-text-left">{data.side}</td>
-    <td class="has-text-left">{data.qty}</td>
+    <td class="has-text-left"
+        >{data.quantity_bought >= 0 ? "bought" : "sold"}</td>
+    <td class="has-text-left">{data.quantity_bought}</td>
     <td class="has-text-right">{data.price}</td>
-    <td class="has-text-right">{data.totalVal}</td>
+    <td class="has-text-right">{data.price * data.quantity_bought}</td>
     <td class="has-text-left has-text-{color}">{data.status}</td>
 </tr>
