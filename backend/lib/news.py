dad60b8ec1f85ed267b1c1ee958e160a828c8eca
Simon <luo.simon@outlook.com>
2023-02-14 12:25:40 +0000

    Clearer variable names

--- backend/lib/news.py
+++ backend/lib/news.py
@@ -1,5 +1,6 @@
 """
 Gets annotated news articles for stocks. Uses TextBlob to perform sentiment analysis.
+Scroll to bottom to see usage and testing.
 """
 
 from textblob import TextBlob
@@ -11,17 +12,17 @@ def get_news(ticker: str, N: int) -> list[TickerArticle]:
     """
     poly = PolygonAPI()
     news = poly.get_news(ticker=ticker, max_items=N)
-    for ta in news:
-        ta.score = get_text_score(ta)
+    for article in news:
+        article.score = get_text_score(article)
     return news
 
 
-def get_text_score(ta: TickerArticle) -> float:
+def get_text_score(tickerArticle: TickerArticle) -> float:
     """
     Returns our text-based NLP score for a given article, between -1 and 1,
     but more likely a very small number near 0
     """
-    t = TextBlob(" ".join([ta.title, ta.description]))
+    t = TextBlob(" ".join([tickerArticle.title, tickerArticle.description]))
     score = t.polarity * t.subjectivity
     assert isinstance(score, float)
     return score
@@ -30,4 +31,4 @@ def get_text_score(ta: TickerArticle) -> float:
 if __name__ == "__main__":
     news = get_news("AAPL", 10)
     for n in news:
-        print(f"{n.score} + {n.title}")
+        print(f"{n.score:.2f} || {n.title[:40]}... \n\t\t -> {n.url[:40]}...")
