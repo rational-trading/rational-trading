d40d58d597f5f16521f5e2dc5e79a092da1a50fc
Simon <47718189+luo-simon@users.noreply.github.com>
2023-02-16 11:32:04 +0000

    54 text nlp integration with polygon api (#57)
    * Initial integration of nlp with polygon api
    
    * Rename file, and improve integration
    
    * Elaborate on docstring
    
    * Spacing
    
    * Clearer variable names
    
    * Rename back to nlp
    
    * Move functionality from nlp.py into polygon_api.py
    
    * Fix typing
    
    * Add publisher field, fix typing
    
    * Fix NLP formula
    
    * Remove unncessary assertions
    
    * Fix link

--- backend/lib/polygon_api.py
+++ backend/lib/polygon_api.py
@@ -1,6 +1,6 @@
 """
 Polygon API class to get stock financials and news articles
-https://polygon.io/docs/stocks/
+https://polygon.io/docs
 https://polygon-api-client.readthedocs.io/en/latest/index.html
 """
 from http.client import HTTPResponse
@@ -11,16 +11,20 @@ from polygon.rest.models import TickerNews
 
 from config.env import env
 
+from lib.nlp import get_text_score
+
 
 class TickerArticle():
-    def __init__(self, title: str, description: str, url: str, published: str) -> None:
+    def __init__(self, title: str, description: str, url: str, date: str, publisher: str) -> None:
         self.title = title
         self.description = description
         self.url = url
-        self.published = published
+        self.date = date
+        self.publisher = publisher
+        self.score = get_text_score(" ".join([title, description]))
 
     def __repr__(self) -> str:
-        return (f"TickerArticle Object ({self.title[:20]}...)")
+        return (f"TickerArticle({self.title[:20]}...)")
 
 
 class TickerFinancials():
@@ -42,29 +46,37 @@ class PolygonAPI():
         return f
 
     def get_news(self, ticker: str, max_items: int) -> list[TickerArticle]:
+        """
+        Returns a list of N annotated news articles (i.e. 'TickerArticle's)
+        """
         news_generator: Iterator[TickerNews] | HTTPResponse = self.client.list_ticker_news(
             ticker=ticker, limit=1)
         articles = []
+
         for _ in range(max_items):
             n = news_generator.__next__()
             # We only get bytes if calling list_ticker_news with raw=True, so can assert TickerNews
             assert isinstance(n, TickerNews)
+            
+            assert isinstance(n.title, str)
+            assert isinstance(n.article_url, str)
+            assert isinstance(n.published_utc, str)
+            
+            desc = n.description if n.description else ""
+            
+            assert n.publisher is not None
+            assert isinstance(n.publisher.name, str)
 
-            assert n.title is not None
-            assert n.description is not None
-            assert n.article_url is not None
-            assert n.published_utc is not None
-
-            article = TickerArticle(n.title, n.description,
-                                    n.article_url, n.published_utc)
+            article = TickerArticle(n.title, desc, n.article_url,
+                                    n.published_utc, n.publisher.name)
             articles.append(article)
+
         return articles
 
 
 # Testing
 if __name__ == "__main__":
     api = PolygonAPI()
-    n = api.get_news("AAPL", 1)
-    f = api.get_financials("AAPL")
-    print(n)
-    print(f)
+    news = api.get_news("AAPL", 10)
+    for n in news:
+        print(f"{n.score:.2f} || {n.publisher} - {n.title[:40]}... \n\t\t -> {n.url[:40]}...")
