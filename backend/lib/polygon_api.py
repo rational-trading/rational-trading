6027c2efc3d708a522a27f80771d3b4e029030e9
Simon <luo.simon@outlook.com>
2023-02-16 12:38:30 +0000

    Improve debugging print messages

--- backend/lib/polygon_api.py
+++ backend/lib/polygon_api.py
@@ -25,7 +25,7 @@ class TickerArticle():
         self.tickers = tickers
 
     def __repr__(self) -> str:
-        return f"{self.score:>6.3f} || {self.publisher[:8]:<8}... {self.title[:40]}..."
+        return f"{self.score:>6.3f} | {self.date[5:7]}/{self.date[8:10]} {self.date[11:16]} | {self.publisher[:8]:<8}... {self.title[:40]}..."
 
 
 class TickerFinancials():
--- backend/lib/precompute.py
+++ backend/lib/precompute.py
@@ -3,9 +3,13 @@ from lib.polygon_api import PolygonAPI
 def main() -> None:
     tickers = ["AAPL", "MSFT", "AMZN,", "NVDA", "GOOGL", "TSLA", "BRK.B", "GOOG", "XOM", "UNH", "JNJ", "JPM", "META", "V", "PG", "HD", "MA", "CVX", "MRK", "LLY"]
     p = PolygonAPI()
-    N = 20
+    N = 10
     articles = p.get_recent_news(N, tickers)
+    print("By published date:")
+    for a in articles:
+        print(a)
     articles.sort(key=lambda x: x.score, reverse=True)
+    print("By NLP score:")
     for a in articles:
         print(a)
 
