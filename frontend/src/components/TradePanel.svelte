20e8f6073e4cb4bb6e677fa3bb5cfa32c7cc53b6
katelkq <kateliangkaiqing@outlook.com>
2023-02-23 12:51:11 +0000

    More details to trade panel

--- frontend/src/components/TradePanel.svelte
+++ frontend/src/components/TradePanel.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
+    import type { Stock } from "$lib/types";
+
     export let close: () => void;
+    let stock: Stock = {
+        ticker: "AAPL",
+        name: "Apple Inc",
+        exchange: "NASDAQ",
+    };
 
     let buy = true;
+    let units: number;
+    $: validUnits = !((units !== undefined && isNaN(units)) || units < 0.01);
 </script>
 
 <div class="modal is-active">
@@ -18,16 +27,23 @@
             </div>
 
             <div class="columns">
+                <!-- trading stats -->
                 <div
-                    class="column is-one-third"
+                    class="column is-one-third ml-3"
                     style="height: 75vh; border-right: 1px solid #4a4a4a;">
+                    <header class="title is-3 mb-0">
+                        {stock.exchange}:{stock.ticker}
+                    </header>
+
+                    <hr style="background: #4a4a4a; height: 1px" />
+
                     <div class="buttons has-addons is-centered">
                         <button
                             class="button is-large {buy
                                 ? 'is-info is-selected'
                                 : ''}"
                             on:click={() => (buy = true)}
-                            style="width: 45%; justify-content: left; text-align: left">
+                            style="height: 8vh; width: 50%; justify-content: left; text-align: left">
                             <div>
                                 <p class="title is-4">Buy</p>
                                 <p class="subtitle is-5">153.22</p>
@@ -38,13 +54,74 @@
                                 ? ''
                                 : 'is-info is-selected'}"
                             on:click={() => (buy = false)}
-                            style="width: 45%; justify-content: right; text-align: right">
+                            style="height: 8vh; width: 50%; justify-content: right; text-align: right">
                             <div>
                                 <p class="title is-4">Sell</p>
                                 <p class="subtitle is-5">152.94</p>
                             </div>
                         </button>
                     </div>
+
+                    <div class="block">
+                        <input
+                            class="input {validUnits ? '' : 'is-danger'}"
+                            type="text"
+                            placeholder="Units"
+                            bind:value={units} />
+
+                        {#if units !== undefined && isNaN(units)}
+                            <p class="has-text-danger">
+                                Please enter a valid number.
+                            </p>
+                        {:else if units < 0.01}
+                            <p class="has-text-danger">
+                                Specified value is less than the minimum of
+                                0.01.
+                            </p>
+                        {/if}
+
+                        <hr style="background: #4a4a4a; height: 1px" />
+
+                        <header class="title is-6">Order info</header>
+
+                        <div
+                            style="display: flex; justify-content: space-between;">
+                            <div style="vertical-align: baseline; ">Units</div>
+                            <div style="vertical-align: baseline; ">
+                                <strong>{validUnits ? units : "-"}</strong>
+                            </div>
+                        </div>
+
+                        <div
+                            style="display: flex; justify-content: space-between;">
+                            <div style="vertical-align: baseline; ">
+                                Per unit value
+                            </div>
+                            <div style="vertical-align: baseline; ">
+                                <strong>-</strong>
+                            </div>
+                        </div>
+
+                        <div
+                            style="display: flex; justify-content: space-between;">
+                            <div style="vertical-align: baseline; ">
+                                Trade value
+                            </div>
+                            <div style="vertical-align: baseline; ">
+                                <strong>-</strong>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- state the reason -->
+                <div class="column">
+                    <p>Random placeholder</p>
+                    {#if buy}
+                        <p>Buy</p>
+                    {:else}
+                        <p>Sell</p>
+                    {/if}
                 </div>
             </div>
         </div>
