a6287fe315f66ab0580da0c75fe7ea072b2d6732
Simon <luo.simon@outlook.com>
2023-02-23 12:52:11 +0000

    Make /news/ public (no auth)

--- backend/endpoints/news/route.py
+++ backend/endpoints/news/route.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
-from ninja import Router, Schema
+from ninja import Router
 from django.http.request import HttpRequest
-from endpoints.auth import AuthBearer
 
 from lib.polygon_api import PolygonAPI, normalise_scores
 
-router = Router(auth=AuthBearer())
+router = Router()
 
 @dataclass
 class TickerArticleDataclass:
@@ -20,21 +19,16 @@ class TickerArticleDataclass:
 @dataclass
 class NewsResponse:
     news: list[TickerArticleDataclass]
-    authenticated_user: str
-
-class AuthenticatedRequest(HttpRequest):
-    auth: str
 
 @router.get("/news/", response=NewsResponse)
-def news(request: AuthenticatedRequest, ticker: str, n=20) -> NewsResponse:
+def news(request: HttpRequest, ticker: str, n=20) -> NewsResponse:
     """
-    /news/ticker=?n=? 
-    Gets the n most recent articles about ticker, as a list of TickerArticles
+    Gets the list of the n most recent articles about ticker
     """
     api = PolygonAPI()
     news = normalise_scores(api.get_news(ticker, n))
     response = []
     for n in news:
         response.append(TickerArticleDataclass(n.title, n.description, n.url, n.date, n.publisher, n.score, n.tickers))
-    return NewsResponse(news=response, authenticated_user=request.auth)
+    return NewsResponse(news=response)
 
