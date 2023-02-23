2e080ac0dfd03fba0bcacdc5c2a095995737535e
Trevor Wang <moonshinetrevor@gmail.com>
2023-02-23 11:19:05 +0000

    fixed typing

--- backend/lib/normalised_final_score.py
+++ backend/lib/normalised_final_score.py
@@ -1,22 +1,24 @@
 
+from typing import List
 import lib.scoring
 import lib.numerical_precompute
 
 
-
-def normalised_numerical_scoring(ticker, numerical_pre_results):
+def normalised_numerical_scoring(ticker: str, numerical_pre_results: List[float]) -> float:
     result = lib.scoring.numerical_scoring(ticker)
-    score = 0
+    score = 0.
     for x in numerical_pre_results:
-        if result > x: score += 0.02
+        if result > x:
+            score += 0.02
     return score
 
 
-
 def final_scoring(ticker: str) -> float:
-    numerical_score = normalised_numerical_scoring(ticker, lib.numerical_precompute.numerical_results)
+    numerical_score = normalised_numerical_scoring(
+        ticker, lib.numerical_precompute.numerical_results)
     # @Simon
     nlp_score = 0
     return 0.5 * numerical_score + 0.5 * nlp_score
+
 
 print(final_scoring("AAPL"))
