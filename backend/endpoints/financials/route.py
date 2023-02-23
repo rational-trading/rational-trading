9c7b37ff0005e240138522f9fd4c880ea2451a40
Miles Watson <contact@mileswatson.net>
2023-02-23 13:12:24 +0000

    Fixed schema issues.

--- backend/endpoints/financials/route.py
+++ backend/endpoints/financials/route.py
@@ -6,9 +6,14 @@ from lib.scoring import get_financial_endpoints
 
 router = Router()
 
-
+class FinancialsResponse(Schema):
+    price_earning_ratio: float
+    earnings_per_share: float
+    debt_to_equity: float
+    current_ratio: float
+        
 @dataclass
-class FinancialsResponseSchema:
+class FinancialsResponse:
     price_earning_ratio: float
     earnings_per_share: float
     debt_to_equity: float
@@ -16,10 +21,10 @@ class FinancialsResponseSchema:
 
 
 @router.get("/", response=FinancialsResponseSchema)
-def maths(request: HttpRequest, ticker: str) -> FinancialsResponseSchema:
+def maths(request: HttpRequest, ticker: str) -> FinancialsResponse:
     print(type(request))
     end_dict = get_financial_endpoints(ticker)
-    return FinancialsResponseSchema(price_earning_ratio=end_dict["price_earning_ratio"],
+    return FinancialsResponse(price_earning_ratio=end_dict["price_earning_ratio"],
                                     earnings_per_share=end_dict["earnings_per_share"],
                                     debt_to_equity=end_dict["debt_to_equity"],
                                     current_ratio=end_dict["current_ratio"])
