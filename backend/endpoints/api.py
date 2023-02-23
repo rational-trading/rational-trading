c8c4de102374b4fdfbde20c0c7bcd2ac18beec5c
Miles Watson <contact@mileswatson.net>
2023-02-23 18:02:37 +0000

    Restructured.

--- backend/endpoints/api.py
+++ backend/endpoints/api.py
@@ -14,6 +14,7 @@ from endpoints.error import FriendlyException
 
 from .demo.route import router as demo_router
 from .hello.route import router as hello_router
+from .auth.route import router as auth_router
 from .maths.route import router as maths_router
 from .price.route import router as price_router
 from .portfolio.route import router as portfolio_router
@@ -24,9 +25,7 @@ from models.models import UserModel
 
 api = NinjaAPI()
 
-api.add_router("/demo/", demo_router)
-api.add_router("/hello/", hello_router)
-api.add_router("/maths/", maths_router)
+api.add_router("/auth/", auth_router)
 api.add_router("/price/", price_router)
 api.add_router("/portfolio/", portfolio_router)
 api.add_router("/financials/", financials_router)
@@ -51,64 +50,6 @@ def validation_error(request: HttpRequest, e: ValidationError) -> HttpResponse:
         {"error": "Request validation failed. Please use the correct input format."},
         status=422
     )
-
-
-class TokenSchema(Schema):
-    access_token: str
-
-
-class UserInput(Schema):
-    username: str
-    password: str
-
-
-class SignupSuccess(Schema):
-    success: bool
-
-
-@api.post("/signup", auth=None)
-def createAccount(request: HttpBearer, data: UserInput) -> TokenSchema:
-
-    try:
-        UserModel.objects.get(username=data.username)
-        raise AuthenticationError("Username has been taken.")
-    except UserModel.DoesNotExist:
-        UserModel.create_typed(
-            username=data.username, password=make_password(data.password), balance=0.)
-        return TokenSchema(access_token=create_token(data.username))
-
-
-@api.post("/login", auth=None, response=TokenSchema)
-# union
-def auth(request: HttpBearer, data: UserInput) -> TokenSchema:
-    # check username
-    # check password
-    pwdHash = UserModel.objects.get(username=data.username).password
-    if (check_password(data.password, pwdHash)):
-
-        token = create_token(data.username)
-        return TokenSchema(access_token=token)
-    else:
-        raise AuthenticationError()
-
-
-def create_token(username: str) -> str:
-
-    JWT_SIGNING_KEY: str = env("JWT_SIGNING_KEY")
-    JWT_ACCESS_EXPIRY: int = env("JWT_ACCESS_EXPIRY")
-    access_expire = datetime.datetime.now(
-        datetime.timezone.utc) + datetime.timedelta(minutes=int(JWT_ACCESS_EXPIRY))
-    encoded_access_jwt = jwt.encode(
-        {"sub": username, "exp": access_expire}, JWT_SIGNING_KEY, algorithm="HS256")
-    return encoded_access_jwt
-
-
-"""example
-@api.get("/something", auth=AuthBearer())
-def something(request):
-    ...
-
-"""
 
 
 @api.exception_handler(FriendlyException)
---
+++ backend/endpoints/auth/route.py
@@ -0,0 +1,69 @@
+import datetime
+from ninja import Schema, Router
+from ninja.errors import AuthenticationError
+from django.contrib.auth.hashers import make_password, check_password
+from django.http.request import HttpRequest
+import jwt
+
+from models.models import UserModel
+from config.env import env
+
+router = Router()
+
+
+class TokenSchema(Schema):
+    access_token: str
+
+
+class UserInput(Schema):
+    username: str
+    password: str
+
+
+class SignupSuccess(Schema):
+    success: bool
+
+
+@router.post("/signup", auth=None)
+def signup(request: HttpRequest, data: UserInput) -> TokenSchema:
+
+    try:
+        UserModel.objects.get(username=data.username)
+        raise AuthenticationError("Username has been taken.")
+    except UserModel.DoesNotExist:
+        UserModel.create_typed(
+            username=data.username, password=make_password(data.password), balance=0.)
+        return TokenSchema(access_token=create_token(data.username))
+
+
+@router.post("/login", auth=None, response=TokenSchema)
+# union
+def login(request: HttpRequest, data: UserInput) -> TokenSchema:
+    # check username
+    # check password
+    pwdHash = UserModel.objects.get(username=data.username).password
+    if (check_password(data.password, pwdHash)):
+
+        token = create_token(data.username)
+        return TokenSchema(access_token=token)
+    else:
+        raise AuthenticationError()
+
+
+def create_token(username: str) -> str:
+
+    JWT_SIGNING_KEY: str = env("JWT_SIGNING_KEY")
+    JWT_ACCESS_EXPIRY: int = env("JWT_ACCESS_EXPIRY")
+    access_expire = datetime.datetime.now(
+        datetime.timezone.utc) + datetime.timedelta(minutes=int(JWT_ACCESS_EXPIRY))
+    encoded_access_jwt = jwt.encode(
+        {"sub": username, "exp": access_expire}, JWT_SIGNING_KEY, algorithm="HS256")
+    return encoded_access_jwt
+
+
+"""example
+@api.get("/something", auth=AuthBearer())
+def something(request):
+    ...
+
+"""
