adf29ff4bc62670e8932cebaea80edefcbb54707
Stefan Pitur <stefan.pitur24@gmail.com>
2023-02-24 11:43:36 +0000

    creating Logout component

---
+++ frontend/src/components/Logout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { authenticated } from "$lib/stores";
+
+    function handleLogout() {
+        authenticated.set(false);
+        localStorage.removeItem("access_token");
+    }
+</script>
+
+<button class="button is-info" on:click={() => handleLogout()}>
+    <strong>Log out</strong>
+</button>
