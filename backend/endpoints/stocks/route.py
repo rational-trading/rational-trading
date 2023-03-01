e70b54a6ca66c76cc9322d2afe06f6fa5c7c19e7
Stefan Pitur <stefan.pitur24@gmail.com>
2023-03-01 14:34:49 +0000

    update StockModel's company name and exchange if not already added

--- backend/endpoints/stocks/route.py
+++ backend/endpoints/stocks/route.py
@@ -2,6 +2,7 @@ from typing import List
 from ninja import Router, Schema
 from django.db import transaction
 from django.http.request import HttpRequest
+from models.models import StockModel
 
 from lib.polygon_api import PolygonAPI, TickerDetails
 
@@ -17,10 +18,20 @@ class TickerDetailsSchema(Schema):
 @router.get("/all", response=List[TickerDetailsSchema])
 @transaction.atomic
 def all_stocks(request: HttpRequest) -> List[TickerDetails]:
-    companies_ticker = ["AAPL", "META", "ADBE"]
     api = PolygonAPI()
+    stocks = StockModel.objects.all()
 
     stocks_details = []
-    for company_ticker in companies_ticker:
-        stocks_details.append(api.get_ticker_details(company_ticker))
+    for stock in stocks:
+        if (stock.company_name == "" or stock.exchange == ""):
+            tickerDetails = api.get_ticker_details(stock.ticker)
+
+            stock.company_name = tickerDetails.company_name
+            stock.exchange = tickerDetails.exchange
+            stock.save()
+
+            stocks_details.append(tickerDetails)
+        else:
+            stocks_details.append(TickerDetails(
+                ticker=stock.ticker, company_name=stock.company_name, exchange=stock.exchange))
     return stocks_details
