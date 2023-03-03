92946b1ceb3e123e0a5c827809077b3eb90f8c01
katelkq <kateliangkaiqing@outlook.com>
2023-03-03 17:49:16 +0000

    getMonth() counts from 0

--- frontend/src/lib/functions.ts
+++ frontend/src/lib/functions.ts
@@ -17,7 +17,7 @@ export function convertUnixDate(unixDate: number): string {
     const date = new Date(unixDate);
 
     const year = `${date.getFullYear()}`;
-    const month = `0${date.getMonth()}`.slice(-2);
+    const month = `0${date.getMonth() + 1}`.slice(-2);
     const day = `0${date.getDate()}`.slice(-2);
 
     const hour = `0${date.getHours()}`.slice(-2);
