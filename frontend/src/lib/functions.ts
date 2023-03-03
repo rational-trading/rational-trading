4db1821483142957643deb1ce75fd3050697d30e
Stefan - Alexandru Pitur <31697875+StefanPitur@users.noreply.github.com>
2023-03-03 17:13:04 +0000

    85 connect portfolio UI with backend (#90)
    * rounding calculations to 2 decimals
    
    * Details section connected with backend - still some hardcoded values
    
    * connected details section with backend holdings and supported stocks
    
    * fixing lint errors
    
    * connect activity section with backend trades
    
    * connect sumary section with backend portfolio status
    
    * changed currency to USD and modified Asset component to make use of available functions
    
    * removed unnecessary spaces from routing annotators

--- frontend/src/lib/functions.ts
+++ frontend/src/lib/functions.ts
@@ -13,6 +13,29 @@ export function capitalize(s: string): string {
     return s[0].toUpperCase() + s.slice(1);
 }
 
+export function convertUnixDate(unixDate: number): string {
+    const date = new Date(unixDate);
+
+    const year = `${date.getFullYear()}`;
+    const month = `0${date.getMonth()}`.slice(-2);
+    const day = `0${date.getDate()}`.slice(-2);
+
+    const hour = `0${date.getHours()}`.slice(-2);
+    const minute = `0${date.getMinutes()}`.slice(-2);
+    const second = `0${date.getSeconds()}`.slice(-2);
+
+    const formatedDate = `${year}-${month}-${day} ${hour}:${minute}:${second}`;
+    return formatedDate;
+}
+
+export function convertValueToMoney(value: number): string {
+    return `$${new Intl.NumberFormat().format(Math.round(value * 100) / 100)}`;
+}
+
+export function calculatePercentage(a: number, b: number) {
+    return `${((a / b) * 100).toFixed(2)}%`;
+}
+
 // may need to call the wrappers more than once, triggered by change in store value
 let stocksMap: Map<string, Stock> | null;
 stocksDetails.subscribe((x) => { stocksMap = x; });
