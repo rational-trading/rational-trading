1ddc0614dd11febe510f0a0a96c85918cd5e84c2
Simon <luo.simon@outlook.com>
2023-03-07 12:58:28 +0000

    Fix

--- backend/endpoints/trades/route.py
+++ backend/endpoints/trades/route.py
@@ -38,7 +38,7 @@ class TradeSchema(Schema):
             text_evidence=model.text_evidence,
             article_evidence=article_evidence,
             evidence=trade_score_evidence(model.text_evidence, article_evidence),
-            controversy=trade_score_controversy(),
+            controversy=trade_score_controversy(model.stock.ticker),
             financial_risk=trade_score_financial_risk(model.stock.ticker))
 
 
